185b71ae2910cc881c68ad39b099b42d91575d5a
Rishabh Agarwal <Rishabh130498@gmail.com>
2021-01-19 03:29:13 +0530

    Input tag is changed to controlled, onChange and OnSubmit props introduced

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,30 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { term: "" }; //to make controlled input tag
+
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    //TODO call a callback
+    // from app component
+  };
+
   render() {
     return (
-      <div className="ui Segment">
-        <form className="ui form">
+      <div className="ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="ui field">
             <label>Video Search</label>
-            <input type="text" />
+            <input
+              onChange={this.onInputChange}
+              type="text"
+              value={this.state.term} //to store changes in component not in DOM
+            />
           </div>
         </form>
       </div>
