d91864d60168182f4186164dda2d12266d6e8a80
Rishabh Agarwal <Rishabh130498@gmail.com>
2021-01-19 03:38:37 +0530

    VideoList component that will take search term from App.js and make a search from YT API

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import VideoList from "./VideoList";
 
 class App extends React.Component {
   render() {
     return (
       <div className="ui container">
         <SearchBar />
+        <VideoList />
       </div>
     );
   }
