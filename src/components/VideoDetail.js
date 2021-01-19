0d1d16b15f6f632a559531451dfd0d9f2ecd724b
Rishabh Agarwal <Rishabh130498@gmail.com>
2021-01-19 21:46:00 +0530

    some styling to show selectedVideo detail in VideoDetail component and iframe implemented

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -7,8 +7,10 @@ const VideoDetail = ({ video }) => {
 
   const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
   return (
-    <div className="ui embed">
-      <iframe src={videoSrc} />
+    <div>
+      <div className="ui embed">
+        <iframe src={videoSrc} />
+      </div>
       <div className="ui segment">
         <div className="ui header">{video.snippet.title}</div>
         <p>{video.snippet.description}</p>
