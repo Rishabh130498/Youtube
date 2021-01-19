3073577743c95270649a6fe3758ac11d148a55a8
Rishabh Agarwal <Rishabh130498@gmail.com>
2021-01-19 14:40:35 +0530

    introduced videos state in App component, we take the responses from YT api and update responses on video state to render list of videos

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,8 @@ import SearchBar from "./SearchBar";
 import VideoList from "./VideoList";
 
 class App extends React.Component {
+  state = { videos: [] };
+
   onTermSubmit = async (term) => {
     const Response = await YtClient.get("/search", {
       params: {
@@ -11,14 +13,14 @@ class App extends React.Component {
       },
     });
 
-    console.log(Response.data.items);
+    this.setState({ videos: Response.data.items });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList />
+        <VideoList />I have {this.state.videos.length} videos.
       </div>
     );
   }
